2ef6bd77d8c9380fb6ccf7b29271a4f8f5a54e92
Dmitrii-Weaver <cordies1106@gmail.com>
2021-02-02 12:46:31 +0300

    added filtering by category and location added tests for the item component

--- components/items.js
+++ components/items.js
@@ -18,7 +18,7 @@ let items_data = {
             name: "Priceless piece of art",
             description: "This piece of art is priceless. Not for sale, just showing off.",
             category: "art",
-            location: "Oulu",
+            location: "oulu",
             images: {},
             price: "priceless",
             date_of_posting: "tomorrow",
@@ -30,6 +30,25 @@ let items_data = {
             id: "1106"
         }
     },
+    {
+        item_id: 2,
+        item_info: {
+            name: "Worthless piece of art",
+            description: "This piece of art is worthless. Just take it.",
+            category: "art",
+            location: "oulu",
+            images: {},
+            price: "0e",
+            date_of_posting: "yesterday",
+            delivery: "pick up"
+        },
+        item_seller: {
+            name: "Dmitrii",
+            phone: "1",
+            id: "1106"
+        }
+    },
+    
   ]}
 
 
@@ -58,10 +77,55 @@ router.get(`/:id`, (req,res) => {
     }
 })
 
+router.get(`/category/:category`, (req,res) => {
+    const result = items_data.items.filter(i => i.item_info.category == req.params.category)
+    if(result === undefined)
+    {
+        res.sendStatus(404)
+    }
+    else
+    {
+        res.json(result);
+    }
+})
+
+router.get(`/location/:location`, (req,res) => {
+    const result = items_data.items.filter(i => i.item_info.location == req.params.location)
+    if(result === undefined)
+    {
+        res.sendStatus(404)
+    }
+    else
+    {
+        res.json(result);
+    }
+})
+
+
 
 router.delete('/:id', (req,res) => {
     items_data.items = items_data.items.filter(item => item.item_id != req.params.id)
     res.sendStatus(200)
+})
+
+router.put('/:id', (req,res) => {
+    const result = items_data.items.find(i => {
+        if(i.item_id == req.params.id){
+            return true
+        }
+        else{
+            return false
+        }
+    })
+    if(result === undefined)
+    {
+        res.sendStatus(404)
+    }
+    else
+    {
+
+        res.json(result);
+    }
 })
 
 
@@ -72,7 +136,7 @@ router.post("/createItem", (req,res) =>{
     const valid = validate(req.body)
     if(valid == true ){
         items_data.items.push(req.body)
-        res.send("ok")
+        res.sendStatus(200)
     }
     else{
         res.sendStatus(400)
--- test/basicTest.js
+++ test/basicTest.js
@@ -16,6 +16,7 @@ describe('test#1 : create user, login and basic functions',  function(){
         server.stop()
     })
 
+
     describe('test route /', function(){
         it("should return an ok code", async function(){
             await chai.request('http://localhost:3000').get('/')
@@ -27,6 +28,8 @@ describe('test#1 : create user, login and basic functions',  function(){
             })
         })
     })
+
+
 
     describe('test route  /register', function(){
         it("should register a user and return an 201 code", async function(){
@@ -75,6 +78,102 @@ describe('test#1 : create user, login and basic functions',  function(){
         })
 
         */
+
+       describe('test route  /items', function(){
+        it("should create an item", async function(){
+            await chai.request('http://localhost:3000')
+            .post('/items/createItem')
+            .send({
+                "item_id": 3,
+                "item_info": {
+                    "name": "ak-74M",
+                    "description": "an alright, not stolen AK-74M",
+                    "category": "tools",
+                    "location": "Oulu",
+                    "images": {},
+                    "price": "100e",
+                    "date_of_posting": "today",
+                    "delivery": "pick up "
+                },
+                "item_seller": {
+                    "name": "boris",
+                    "phone": "12313",
+                    "id": "1"
+                }
+            })
+            .then(response =>{
+                expect(statusCode).to.equal(200)
+            })
+            .catch(error =>{
+
+            })
+        })
+    
+    
+        it("shows all items", async function(){
+            await chai.request('http://localhost:3000')
+            .get('/items')
+            .then(res =>{
+                console.log(res.body)
+                expect(statusCode).to.equal(200)
+            })
+            .catch(error =>{
+            })
+        }) 
+        it("shows items with an id '1' ", async function(){
+            await chai.request('http://localhost:3000')
+            .get('/items/1')   
+            .then(res =>{
+                console.log(res.body)
+                expect(statusCode).to.equal(200)
+            })
+            .catch(error =>{
+            })
+        })
+        it("shows items in category 'art' ", async function(){
+            await chai.request('http://localhost:3000')
+            .get('/items/category/art')   
+            .then(res =>{
+                console.log(res.body)
+                expect(statusCode).to.equal(200)
+            })
+            .catch(error =>{
+            })
+        })
+        it("shows items in location 'Oulu' ", async function(){
+            await chai.request('http://localhost:3000')
+            .get('/items/location/oulu')   
+            .then(res =>{
+                console.log(res.body)
+                expect(statusCode).to.equal(200)
+            })
+            .catch(error =>{
+            })
+        })
+        it("deletes an item with id '1'  ", async function(){
+            await chai.request('http://localhost:3000')
+            .delete('/items/1')   
+            .then(res =>{
+                expect(statusCode).to.equal(200)
+            })
+            .catch(error =>{
+            })
+        })
+        it("shows all items, now without the deleted item", async function(){
+            await chai.request('http://localhost:3000')
+            .get('/items')
+            .then(res =>{
+                console.log(res.body)
+                expect(statusCode).to.equal(200)
+            })
+            .catch(error =>{
+            })
+        }) 
+
+        
+    })
+    
+
 
 })
 
